69fd004d66bac533c68958ee71a6b116824d2635
smolkomartin1 <70076265+smolkomartin1@users.noreply.github.com>
2022-03-05 20:12:10 +0000

    Added automatic admin elevation

---
+++ DisableGoogleDriveCheckmarks.bat
@@ -0,0 +1,62 @@
+::::::::::::::::::::::::::::::::::::::::::::::::
+:: Disable Google Drive checkmarks on folders V1
+::::::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~dpnx0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+ 
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+@Echo Off
+cd %systemroot%\system32
+Cls & ECHO **************************************
+ECHO Removing Google Drive checkmarks...
+ECHO **************************************
+@ECHO off
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\    GoogleDriveCloudOverlayIconHandler" /ve /t REG_SZ /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\    GoogleDriveMirrorBlacklistedOverlayIconHandler" /ve /t REG_SZ /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\    GoogleDrivePinnedOverlayIconHandler" /ve /t REG_SZ /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers\    GoogleDriveProgressOverlayIconHandler" /ve /t REG_SZ /d "0" /f
+ECHO **************************************	
+ECHO Google Drive checkmarks successfully removed^^! Have a nice day^^!
+pause
+Exit
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Special thanks to Matt - https://stackoverflow.com/users/1016343/matt
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
